80d558cb6e6485faab48f670f55939c8f5527495
Anton Miselaytes <5480808+miselaytes-anton@users.noreply.github.com>
2023-10-31 16:10:25 +0100

    use go-cmp/cmp package for tests

--- server/go.mod
+++ server/go.mod
@@ -8,6 +8,7 @@ require github.com/lib/pq v1.10.9
 
 require (
 	github.com/go-echarts/go-echarts/v2 v2.2.7 // indirect
+	github.com/google/go-cmp v0.6.0
 	github.com/gorilla/websocket v1.5.0 // indirect
 	golang.org/x/net v0.15.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
--- server/messageProcessor/measurementHandler_test.go
+++ server/messageProcessor/measurementHandler_test.go
@@ -3,6 +3,7 @@ package messageprocessor
 import (
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	models "github.com/miselaytes-anton/tatadata/server/models"
 )
 
@@ -27,35 +28,13 @@ func Test_parseMeasurementMessage(t *testing.T) {
 		Humidity:    60.22,
 	}
 
-	const float64EqualityThreshold = 1e-9
-
-	if m.CO2-expected.CO2 > float64EqualityThreshold {
-		t.Errorf("CO2: expected %f, got %f", expected.CO2, m.CO2)
-	}
-
-	if m.IAQ-expected.IAQ > float64EqualityThreshold {
-		t.Errorf("IAQ: expected %f, got %f", expected.IAQ, m.IAQ)
-	}
-
-	if m.VOC-expected.VOC > float64EqualityThreshold {
-		t.Errorf("VOC: expected %f, got %f", expected.VOC, m.VOC)
-	}
-
-	if m.Pressure != expected.Pressure {
-		t.Errorf("Pressure: expected %f, got %f", expected.Pressure, m.Pressure)
-	}
-
-	if m.Temperature-expected.Temperature > float64EqualityThreshold {
-		t.Errorf("Temperature: expected %f, got %f", expected.Temperature, m.Temperature)
-	}
-
-	if m.Humidity-expected.Humidity > float64EqualityThreshold {
-		t.Errorf("Humidity: expected %f, got %f", expected.Humidity, m.Humidity)
+	if diff := cmp.Diff(expected, m); diff != "" {
+		t.Error(diff)
 	}
 
 	_, err = parseMeasurementMessage("")
 
 	if err == nil {
-		t.Errorf("err: expected error to no be nil")
+		t.Errorf("err: expected error to not be nil")
 	}
 }
