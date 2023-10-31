1fb5496b2d80d1a3cea1c07ebeaa80f9992676c3
Anton Miselaytes <5480808+miselaytes-anton@users.noreply.github.com>
2023-10-31 16:27:23 +0100

    use table tests

--- server/messageProcessor/measurementHandler_test.go
+++ server/messageProcessor/measurementHandler_test.go
@@ -8,33 +8,66 @@ import (
 )
 
 func Test_parseMeasurementMessage(t *testing.T) {
-	m, err := parseMeasurementMessage("bedroom 51.86 607.44 0.52 100853 27.25 60.22")
-
-	if err != nil {
-		t.Error("expected error to be nil, got", err)
+	data := []struct {
+		name     string
+		message  string
+		expected models.Measurement
+		errMsg   string
+	}{
+		{
+			"valid message",
+			"bedroom 51.86 607.44 0.52 100853 27.25 60.22",
+			models.Measurement{
+				SensorID:    "bedroom",
+				IAQ:         51.86,
+				CO2:         607.44,
+				VOC:         0.52,
+				Pressure:    100853,
+				Temperature: 27.25,
+				Humidity:    60.22,
+			},
+			"",
+		},
+		{
+			"empty message",
+			"",
+			models.Measurement{},
+			"EOF",
+		},
+		{
+			"invalid message",
+			"bedroom something",
+			models.Measurement{
+				SensorID:    "bedroom",
+				IAQ:         0,
+				CO2:         0,
+				VOC:         0,
+				Pressure:    0,
+				Temperature: 0,
+				Humidity:    0,
+			},
+			"strconv.ParseFloat: parsing \"\": invalid syntax",
+		},
 	}
 
-	if err != nil {
-		t.Error("expected error to be nil, got", err)
-	}
+	for _, d := range data {
+		t.Run(
+			d.name,
+			func(t *testing.T) {
+				m, err := parseMeasurementMessage(d.message)
+				if diff := cmp.Diff(d.expected, m); diff != "" {
+					t.Error(diff)
+				}
 
-	expected := models.Measurement{
-		SensorID:    "bedroom",
-		IAQ:         51.86,
-		CO2:         607.44,
-		VOC:         0.52,
-		Pressure:    100853,
-		Temperature: 27.25,
-		Humidity:    60.22,
-	}
+				var errMsg string
+				if err != nil {
+					errMsg = err.Error()
+				}
 
-	if diff := cmp.Diff(expected, m); diff != "" {
-		t.Error(diff)
-	}
-
-	_, err = parseMeasurementMessage("")
-
-	if err == nil {
-		t.Errorf("err: expected error to not be nil")
+				if errMsg != d.errMsg {
+					t.Errorf("Expected error message `%s`, got `%s`", d.errMsg, errMsg)
+				}
+			},
+		)
 	}
 }
