319fdf41bd052ec6534f49f6beb9be0635db2c41
Anton Miselaytes <5480808+miselaytes-anton@users.noreply.github.com>
2023-11-10 16:57:59 +0100

    add debug, warning, critical loggers

--- backend/internal/log/log.go
+++ backend/internal/log/log.go
@@ -5,10 +5,8 @@ import (
 	"os"
 )
 
-// Use log.New() to create a logger for writing information messages. This takes // three parameters: the destination to write the logs to (os.Stdout), a string // prefix for message (INFO followed by a tab), and flags to indicate what
-// additional information to include (local date and time). Note that the flags // are joined using the bitwise OR operator |.
+var Debug = log.New(os.Stdout, "DEBUG\t", log.Ldate|log.Ltime|log.Lshortfile)
 var Info = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-
-// Create a logger for writing error messages in the same way, but use stderr as // the destination and use the log.Lshortfile flag to include the relevant
-// file name and line number.
+var Warning = log.New(os.Stdout, "WARNING\t", log.Ldate|log.Ltime|log.Lshortfile)
 var Error = log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+var Critical = log.New(os.Stderr, "CRITICAL\t", log.Ldate|log.Ltime|log.Lshortfile)
